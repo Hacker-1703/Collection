55fc618759e13dd9eb185d659b5a20c0d3286ac8
Hacker-1703 <ishmenev.im@gmail.com>
2024-01-23 12:09:39 +0300

    hz

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.lang.reflect.Array;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Date date1 = new Date(1, 1, 2024);
-        Date date2 = new Date(29, 12, 2023);
-        System.out.println("До нового года " + date1.comparisonOfDatesWithoutOutput(date2) + " дня!");
+    public static <T> T[] merge(Class<T> clazz, T[] a, T[] b) {
+        T[] newArray;
+        newArray = (T[]) Array.newInstance(clazz, a.length + b.length);
+        for (int i = 0; i < a.length; i++) {
+            newArray[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            newArray[i + a.length] = b[i];
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"Я", "хочу"};
+        String[] b = {"жестко", "пукнуть"};
+        String[] c = merge(String.class, a, b);
+        for (String i : c){
+            System.out.println(i);
+        }
     }
 }
